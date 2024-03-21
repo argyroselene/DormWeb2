21ca5666de3aebca9f0093f8afc3ebd81fd8667f
Sidratul Muntaha <143277143+argyroselene@users.noreply.github.com>
2024-03-22 00:03:24 +0600

    Update student.h

--- student.h
+++ student.h
@@ -3,6 +3,7 @@
 void draw_border(int length,char symbol);
 void print_centre_wnl(char *s);
 void login_student();
+void study();
 unsigned long encryption(char* passcode);
 struct account{
     char name [120];
