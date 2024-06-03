f5106c780ffb43c9e5501ed3c645bf6dd0a462b5
MaishaNajAlam <161056982+MaishaNajAlam@users.noreply.github.com>
2024-06-03 20:18:55 +0600

    Updated mealtrack.c with csv file

--- mealtrack.c
+++ mealtrack.c
@@ -1,50 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
+
 #define MAX_LENGTH 100
 
-// Function to reset the meal records file
 void resetFileForNewMeal(char *filename) {
     FILE *file = fopen(filename, "w");
     if (file == NULL) {
         printf("Error opening file.\n");
         return;
     }
+    fprintf(file, "MealID\n"); 
     printf("The file is reset.\n");
     fclose(file);
 }
 
-// Function to check if the meal has already been taken
 int mealAlreadyTaken(char *filename, char *studentID) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file.\n");
-        return -1; // Return -1 if there's an error opening the file
+        return -1;
     }
 
     char line[MAX_LENGTH];
     while (fgets(line, MAX_LENGTH, file) != NULL) {
         char id[MAX_LENGTH];
-        sscanf(line, "%[^,]", id);
+        sscanf(line, "%[^,\n]", id);
 
-        // Trim leading and trailing whitespace characters from id
+
         char *ptr = id;
-        while (*ptr == ' ' || *ptr == '\t')
-            ptr++;
+        while (*ptr == ' ' || *ptr == '\t') ptr++;
         for (int i = strlen(ptr) - 1; i >= 0 && (ptr[i] == ' ' || ptr[i] == '\t' || ptr[i] == '\n'); i--)
             ptr[i] = '\0';
 
         if (strcmp(id, studentID) == 0) {
             fclose(file);
-            return 1; // Return 1 if the meal has already been taken
+            return 1;
         }
     }
 
     fclose(file);
-    return 0; // Return 0 if the meal hasn't been taken yet
+    return 0;
 }
 
-// Function to update the meal status in the file
+
 void updateMealStatus(char *filename, char *studentID) {
     FILE *file = fopen(filename, "a");
     if (file == NULL) {
@@ -57,27 +57,77 @@ void updateMealStatus(char *filename, char *studentID) {
     fclose(file);
 }
 
-void mealtrack() {
-    char filename[] = "meal_records.txt";
-    printf("Please choose an option :\n");
-    printf("Update Meal Status (#1)\n");
-    printf("Start a new Meal (#2)\n");
+void mealtrack()
+{
+    char filename[] = "meal_records.csv";
+menu:
+     printf("\n\n\n\n\n\n\n\n\n");
+    center_print("Please choose an option :\n");
+    center_print("Update Meal Status (#1)\n");
+    center_print("Start a new Meal (#2)\n");
+    center_print("Go to Admin menu (#3)\n");
+
 
     int choice;
     scanf("%d", &choice);
 
-    if (choice == 1) {
+    if (choice == 1)
+    {
         char studentID[MAX_LENGTH];
-        printf("Enter student ID: ");
+add:
+        center_print("Enter student ID: ");
         scanf("%s", studentID);
 
-        if (mealAlreadyTaken(filename, studentID)) {
-            printf("Meal has already been taken.\n");
-        } else {
-            updateMealStatus(filename, studentID);
+        if (mealAlreadyTaken(filename, studentID))
+        {
+            center_print("Meal has already been taken.\n");
+            center_print("Please press B or b to Go Back else press any key to continue:\n");
+            char c;
+            scanf(" %c",&c);
+            if(c=='B' || c=='b')
+            {
+                system("cls");
+                goto menu;
+            }
+            else
+            {
+
+                goto add;
+            }
         }
-    } else if (choice == 2) {
+        else
+        {
+            updateMealStatus(filename, studentID);
+            center_print("Please press B or b to go back , else press any other key to continue:\n");
+            char c;
+            scanf(" %c",&c);
+            if(c=='B' || c=='b')
+            {
+                system("cls");
+                goto menu;
+            }
+            else
+            {
+
+                goto add;
+            }
+        }
+    }
+    else if (choice == 2)
+    {
         resetFileForNewMeal(filename);
+
+        center_print("Please press any key to continue:\n");
+            char c;
+            scanf(" %c",&c);
+            system("cls");
+            goto menu;
+
+    }
+    else if(choice==3){
+        system("cls");
+        admin_menu();
     }
 
 }
+
