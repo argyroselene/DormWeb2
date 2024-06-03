9601ee58e63fd58e1ddc9825fb1ba6793ef5222e
MaishaNajAlam <161056982+MaishaNajAlam@users.noreply.github.com>
2024-06-03 21:22:48 +0600

    Updated complaint_submit.c txt to csv

--- complaint_submit.c
+++ complaint_submit.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include"complaint_submit.h"
 
-void complaint_submit() {
+void complaint_submit()
+{
     char type_string[100];
 
     printf("\n\n\n\n\n\n\n\n\n\n");
@@ -11,6 +12,7 @@ void complaint_submit() {
     int room;
     scanf("%d", &room);
     system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n");
     center_print("Please select the type of complaint:");
     center_print("Electrical (#1)");
     center_print("Plumbing (#2)");
@@ -18,69 +20,85 @@ void complaint_submit() {
     center_print("Internet (#4)");
     center_print("Go to home (#0)");
 
-    int what_to_do;
+    char what_to_do;
 
 error:
-    scanf("%d", &what_to_do);
-    switch (what_to_do) {
-    case 1:
+    scanf(" %c", &what_to_do);
+    switch (what_to_do)
+    {
+    case '1':
         system("cls");
         strcpy(type_string, "Electrical");
         break;
 
-    case 2:
+    case '2':
         system("cls");
         strcpy(type_string, "Plumbing");
         break;
 
-    case 3:
+    case '3':
         system("cls");
         strcpy(type_string, "Carpentry");
         break;
 
-    case 4:
+    case '4':
         system("cls");
         strcpy(type_string, "Internet");
         break;
-    case 0:
+    case '0':
         system("cls");
-        home();
-        break;
-
+        return;
     default:
         printf("Invalid Input. Please try again\n");
         goto error;
-        break;
     }
-    // Taking problem and available time as input
+
     char problem[500];
+    printf("\n\n\n\n\n\n\n\n\n\n");
     center_print("Please mention your problem");
     scanf(" %[^\n]", problem);
     system("cls");
+    printf("\n\n\n\n\n\n\n\n\n\n");
     center_print("Please mention your available time and date");
     char availability[150];
     scanf(" %[^\n]", availability);
 
-    // Open file to read the last complaint number
     FILE *file;
-    file = fopen("complaints.txt", "r");
+    file = fopen("complaints.csv", "r");
+    if (file == NULL)
+    {
+        file = fopen("complaints.csv", "w");
+        if (file == NULL)
+        {
+            printf("Error opening file!\n");
+            return;
+        }
+        fprintf(file, "Complaint No,Room,Type,Problem,Availability\n");
+        fclose(file);
+        file = fopen("complaints.csv", "r");
+    }
+
     int last_complaint_no = 0;
     char line[1000];
 
-    // Read the last line of the file to get the last complaint number
-    while (fgets(line, sizeof(line), file) != NULL) {
-        sscanf(line, "Complaint No: %d,", &last_complaint_no);
+    fgets(line, sizeof(line), file);
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        sscanf(line, "%d,", &last_complaint_no);
     }
 
     fclose(file);
 
-    // storing all the info in a file (append)
-    file = fopen("complaints.txt", "a");
+    file = fopen("complaints.csv", "a");
 
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file!\n");
-    } else {
-        fprintf(file, "Complaint No: %d, Room: %d, Type: %s, Problem: %s, Availability: %s\n", last_complaint_no + 1, room, type_string, problem, availability);
+    }
+    else
+    {
+        fprintf(file, "%d,%d,%s,%s,%s\n", last_complaint_no + 1, room, type_string, problem, availability);
         fclose(file);
     }
 
@@ -88,7 +106,7 @@ error:
     center_print("Press any key to continue");
     char temp;
     fflush(stdin);
-    scanf("%c", &temp);
+    scanf(" %c", &temp);
     system("cls");
-    home();
+    student_menu();
 }
