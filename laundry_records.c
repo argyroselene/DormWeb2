c97bbd4ab546defe842e03ee1d2984dee6ea356b
MaishaNajAlam <161056982+MaishaNajAlam@users.noreply.github.com>
2024-06-06 22:24:00 +0600

    Update laundry_records.c

--- laundry_records.c
+++ laundry_records.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
-#include"laundry.h"
+#include <stdlib.h>
+#include <string.h>
+#include "laundry.h"
 
-int laundry_records() {
-    FILE *laundry;
+void writeLaundryToCSV(const char *csvFileName, int laundryNo) {
 
-    laundry = fopen("laundry.txt", "r");
-
-    if (laundry == NULL) {
-        printf("Error opening file.\n");
-        home();
+    FILE *csvFile = fopen(csvFileName, "a");
+    if (csvFile == NULL) {
+        printf("Error opening CSV file for writing!\n");
+        return;
     }
 
-    char line[100];
-    while (fgets(line, sizeof(line), laundry)) {
-        printf("%s", line);
-    }
 
-    fclose(laundry);
+    fprintf(csvFile, "%d\n", laundryNo);
+
+    fclose(csvFile);
+}
+
+void laundry_receive() {
+    int laundry_no;
+    printf("Enter the laundry number to receive: ");
+    scanf("%d", &laundry_no);
+
+    writeLaundryToCSV("laundry.csv", laundry_no);
+
+    printf("Laundry with number %d received successfully.\n", laundry_no);
+
+
+    system("cls");
     center_print("Press (B) or (b) to go back");
     center_print("Press any key to continue");
 
-    char option2;;
-        scanf(" %c",&option2);
+    char option2;
+    scanf(" %c", &option2);
 
-        if(option2 == 'B' || option2 == 'b')
-
-        {
-            system("cls");
-            laundry_main();
-        }
-    else{
-            char temp;
-    fflush(stdin);
-    system("cls");
-    home();
+    if (option2 == 'B' || option2 == 'b') {
+        system("cls");
+        laundry_main();
+    } else {
+        fflush(stdin);
+        system("cls");
+        home();
     }
 }
+
 
